41d38121594fd2ba1cd806674785233c6bf83f2f
RD <r.j.deja@gmail.com>
2023-03-31 16:07:42 +0200

    adding test project

---
+++ IOTest2/IOTest2/CRect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTest2
+{
+    public class CRect
+    {
+        private int w;
+        private int h;
+        public CRect(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+        }
+
+        public int Area()
+        {
+            return w * h;
+        }
+    }
+}
